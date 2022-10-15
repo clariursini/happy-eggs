61d1b9c0210b052d6314bed94900d4f35ae9be35
Clari Ursini <clari_ursini@hotmail.com>
2022-10-15 20:30:43 -0300

    added coudinary photos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  has_many_attached :photos
+  validates :photo, presence: true
+  validates :description, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
 end
