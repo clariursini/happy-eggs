5ea00c195e653971101052ffa9443bf1b807d581
Clari Ursini <clari_ursini@hotmail.com>
2022-10-15 20:18:01 -0300

    updated products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.drestroy
+    redirect_to products_path, status: :see_other
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :photos[])
+  end
+
 end
