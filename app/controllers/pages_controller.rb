346b0be23aa9c8e746fdbf29434a18cc2f6cfb6b
Clari Ursini <clari_ursini@hotmail.com>
2022-10-16 18:08:36 -0300

    updated product new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :contact ]
+  skip_before_action :authenticate_user!, only: [ :index, :show, :new ]
 
   def home
+  end
+
+  def index
+  end
+
+  def show
   end
 end
